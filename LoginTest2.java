1a7c8703ebcd8f3422c34b3207f352d514b66baf
channakeshavalugit <c9108120823@gmail.com>
2022-11-25 09:17:46 +0530

    addproject

---
+++ LoginTest2.java
@@ -0,0 +1,33 @@
+package com.crm.test;
+import java.util.ArrayList;
+import org.testng.annotations.Test;
+import com.crm.base.ExcelReader;
+import com.crm.base.Keywords;
+public class LoginTest2 {
+	@Test
+public void loginTest() {
+	ArrayList testData = ExcelReader.getTestData("D://test1//loginTestCase.xlsx");
+	Keywords Key = new Keywords();
+	for (int i = 0; i < testData.size(); i++) {
+		if(testData.get(i).equals("open")) {
+			Key.open();
+		}
+		if(testData.get(i).equals("url")) {
+      String httpUrl=testData.get(i+1).toString();
+      Key.url(httpUrl);
+		}
+		if(testData.get(i).equals("input")) {
+		      String Data=testData.get(i+1).toString();
+		      String objectName=testData.get(i+2).toString();
+		      Key.input(Data,objectName);
+		}
+		if(testData.get(i).equals("click")) {
+		      String objectName=testData.get(i+2).toString();
+		      Key.click(objectName);
+		}
+		if(testData.get(i).equals("close")) {
+		      Key.close();
+		}
+	}
+}
+}
